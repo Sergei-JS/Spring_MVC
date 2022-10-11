38f53c3e41fb742d3427f0e2bc4ab7ae1f8b8f98
”Ivan <amigo382@mail.ru>
2022-10-11 23:32:20 +0300

    Initial commit

--- src/main/java/spring_mvc/controller.java
+++ src/main/java/spring_mvc/controller.java
@@ -1,4 +1,17 @@
 package spring_mvc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class controller {
+    @RequestMapping(value = "/employee")
+    public class MyController {
+
+        @RequestMapping("/")
+        public String scowFirst() {
+            return "first-view";
+        }
+    }
 }
+
